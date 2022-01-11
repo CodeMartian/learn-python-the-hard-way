70a8b530544ee4d52c3c55a0b4916e5f3fb2eb3f
Jeffrey Martin <jeffrey.martin@thoughtworks.com>
2022-01-11 14:04:28 -0700

    adds exercises #26-#31

---
+++ src/ex29.py
@@ -0,0 +1,26 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print('Too many cats! The world is doomed!')
+
+if people > cats:
+    print('Not many cats! The world is saved!')
+
+if people < dogs:
+    print('So many dogs! I\'m in heaven.')
+
+if people > dogs:
+    print('Not enough dogs. I don\'t want to live in this world.')
+
+dogs += 5
+
+if people >= dogs:
+    print('People are greater than or equal to dogs.')
+
+if people <= dogs:
+    print('People are less than dogs.')
+
+if people == dogs:
+    print('People equals dogs.')
